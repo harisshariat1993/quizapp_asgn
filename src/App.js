c2edf6fbc5ae2b31a5a8d9f267d7261789ad8b75
HarisShariat <haris@inqline.com>
2024-05-23 19:45:51 +0500

    commit done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Quiz from './components/Quiz';
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <Quiz />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Data/QuizData.js
@@ -0,0 +1,25 @@
+export const QuizData = [
+    {
+        question: "Which language runs in a web browser",
+        options: ["Java", "Python", "JavaScript", "HTML"],
+        answer: 3
+    },
+
+    {
+        question: "What does HTML stand for?",
+        options: ["Hyperlinks and Text Markup Language", "Home Tool Markup Language", "Hyper Text Markup Language", "Higher Text Markup Language"],
+        answer: 3
+    },
+
+    {
+        question: "Who is making the Web standards?",
+        options: ["Microsoft", "Google", "Mozrilla", "Opera"],
+        answer: 1
+    },
+
+    {
+        question: "Choose the correct HTML element for the largest heading",
+        options: ["<head>", "<h1>", "<h6>", "heading1"],
+        answer: 2
+    },
+];
